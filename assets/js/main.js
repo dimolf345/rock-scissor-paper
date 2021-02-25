37808074eee7c58a2c9504bb3cdc4e4c5e026d8b
unknown <dimolfetta.ta@gmail.com>
2021-02-25 15:43:13 +0100

    mobile working

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,10 @@
 let playerChoice = document.getElementById('playerChoice');
 let bunttonContainer = document.getElementsByClassName('button-container');
 let gameMatch = document.getElementById('gameMatch');
+let winLose = document.getElementById('winLose');
+let playAgain = document.getElementById('playAgain');
+let scoreValue= localStorage.getItem('score');
+let scoreEl = document.getElementById('score');
 
 const choices = {
     "scissor" : ["paper", "lizard"],
@@ -9,24 +13,28 @@ const choices = {
     "spock" : ["scissor", "rock"] ,
     "rock": ["lizard","scissor"]
 }
-
 playerChoice.addEventListener('click', function(e) {
- startRound(e.target)
+    if(e.target.tagName ==='BUTTON') {
+        startRound(e.target);
+    }
 })
+
+playAgain.addEventListener('click',function() {
+    clearGame();
+});
 
 function startRound(userChoice) {
     let picked = pickButton(userChoice);
     let computerChoice = selectComputerChoice();
     appendComputerChoice(computerChoice);
-    winOrLose(player,computer);
-    updateScore();
-
-
+    let userWins = winOrLose(picked,computerChoice);
+    showWinner(userWins);
+    updateScore(userWins);
 }
 
 function pickButton(button) {
-    let buttonChosed = button;
-    playerChoice.appendChild(buttonChosed);
+    let buttonChosed = button.cloneNode();
+    playerChoice.append(buttonChosed);
     playerChoice.style.display = "none";
     gameMatch.style.display = "flex";
     bunttonContainer[0].append(buttonChosed);
@@ -44,3 +52,47 @@ function appendComputerChoice(computerChoice) {
     bunttonContainer[1].innerHTML = '';
     bunttonContainer[1].appendChild(button); 
 }
+
+function winOrLose(picked,computerChoice) {
+    if (picked===computerChoice) {
+        return;
+    }
+    return (choices[picked].includes(computerChoice));
+}
+
+function showWinner(userWins) {
+    let h1 = winLose.getElementsByTagName('h1')[0];
+    h1.textContent = userWins? "You win" : 
+    (typeof(userWins)==='undefined')? "It's a draw" : "You lose";
+    winLose.style.display = "block";
+}
+
+function clearGame() {
+    winLose.style.display = "none";
+    gameMatch.style.display = "none";
+    playerChoice.style.display = "block";
+}
+
+function updateScore(win) {
+    if (win===undefined) {
+        return;
+    }
+    let newScore = parseInt(scoreValue);
+    (win)? newScore++ : newScore--;
+    scoreEl.textContent = newScore;
+    scoreValue = newScore;
+    localStorage.setItem('score', scoreValue.toString());
+}
+
+
+function setFirstScore() {
+    if (!scoreValue) {
+    scoreValue= 10;
+    localStorage.setItem("score", scoreValue.toString());
+    } else {
+        scoreValue = parseInt(localStorage.getItem('score'));
+    }
+}
+
+setFirstScore();
+scoreEl.textContent=scoreValue.toString();
