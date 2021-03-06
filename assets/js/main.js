dda1350b4bd9679ecf80aa34219e7836b010dcf1
unknown <>
2021-03-06 13:06:34 +0100

    completed project

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,16 @@
 let playerChoice = document.getElementById('playerChoice');
-let bunttonContainer = document.getElementsByClassName('button-container');
+let buttonContainer = document.getElementsByClassName('button-container');
 let gameMatch = document.getElementById('gameMatch');
 let winLose = document.getElementById('winLose');
 let playAgain = document.getElementById('playAgain');
 let scoreValue= localStorage.getItem('score');
 let scoreEl = document.getElementById('score');
+
+
+//blank button is used for winning animation keyframe with CSS since
+//the effect is based on box-shadow property. If set to the chosed
+//.game-btn node it will override the box-shadow;
+let blankButton = '<button class="game-btn blank"></button>';
 
 const choices = {
     "scissor" : ["paper", "lizard"],
@@ -13,31 +19,40 @@ const choices = {
     "spock" : ["scissor", "rock"] ,
     "rock": ["lizard","scissor"]
 }
+
 playerChoice.addEventListener('click', function(e) {
     if(e.target.tagName ==='BUTTON') {
         startRound(e.target);
     }
 })
 
+
 playAgain.addEventListener('click',function() {
     clearGame();
 });
 
+/*Start round does all the game*/ 
 function startRound(userChoice) {
     let picked = pickButton(userChoice);
     let computerChoice = selectComputerChoice();
-    appendComputerChoice(computerChoice);
-    let userWins = winOrLose(picked,computerChoice);
-    showWinner(userWins);
-    updateScore(userWins);
+    const timeout = setTimeout(function() {
+        appendComputerChoice(computerChoice);
+        let userWins = winOrLose(picked,computerChoice);
+        showWinner(userWins);
+        updateScore(userWins);
+    }, 800);
+
 }
 
+/*Pick button function copies the node selected by user
+in the gameMatch div and sets the playerChoice div display 
+property to none*/ 
 function pickButton(button) {
     let buttonChosed = button.cloneNode();
     playerChoice.append(buttonChosed);
     playerChoice.style.display = "none";
     gameMatch.style.display = "flex";
-    bunttonContainer[0].append(buttonChosed);
+    buttonContainer[0].append(buttonChosed);
     return button.dataset.game;
 }
 
@@ -46,13 +61,16 @@ function selectComputerChoice() {
     return Object.keys(choices)[randomNum];
 }
 
+/*Creates a new button for the computer choice*/
 function appendComputerChoice(computerChoice) {
     let button = document.createElement('button');
     button.className = "game-btn " + computerChoice;
-    bunttonContainer[1].innerHTML = '';
-    bunttonContainer[1].appendChild(button); 
+    buttonContainer[1].appendChild(button); 
+
+
 }
 
+/*If there is a draw it return undefined*/
 function winOrLose(picked,computerChoice) {
     if (picked===computerChoice) {
         return;
@@ -60,17 +78,26 @@ function winOrLose(picked,computerChoice) {
     return (choices[picked].includes(computerChoice));
 }
 
+/*Show winner add the h1 and select the winning animation to the 
+correct .button-container div*/ 
+
 function showWinner(userWins) {
     let h1 = winLose.getElementsByTagName('h1')[0];
+    if (userWins !== undefined) {
+        let index = userWins? 0 : 1;
+        let button= buttonContainer[index].getElementsByTagName('button')[0];
+        button.className += ' winning';
+    }
     h1.textContent = userWins? "You win" : 
     (typeof(userWins)==='undefined')? "It's a draw" : "You lose";
-    winLose.style.display = "block";
+    winLose.style.display = "flex";
 }
 
 function clearGame() {
     winLose.style.display = "none";
     gameMatch.style.display = "none";
     playerChoice.style.display = "block";
+    clearButtons();
 }
 
 function updateScore(win) {
@@ -94,5 +121,13 @@ function setFirstScore() {
     }
 }
 
+function clearButtons() {
+    buttonContainer[0].innerHTML = blankButton;
+    buttonContainer[1].innerHTML = blankButton;
+}
+
+
+
 setFirstScore();
 scoreEl.textContent=scoreValue.toString();
+
